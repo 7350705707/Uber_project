d82268ebaf63a3c62fcc1df4fb54d084090cd01c
haresh git <hkjivan735@gmail.com>
2025-05-11 21:14:25 +0530

    In Frontend Captain & User Login & Signup Interface is created

---
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainSignup = () => {
+
+    const [user, setUser] = useState({
+      email: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+    }); 
+    const { email, password, firstname, lastname } = user;
+    const handleOnChange = (e) => {
+      const { name, value } = e.target;
+      setUser((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const submitHandler = async (e) => {
+      e.preventDefault();
+      try {
+        console.log(user);
+      }catch(err) {
+        console.log(err);
+      }finally{
+        setUser({
+          email: "",
+          password: "",
+          firstname: "",
+          lastname: "",
+        })
+      }
+    }
+
+
+
+  return (
+   <div className="p-7 h-screen flex flex-col justify-between ">
+      <div>
+      <img className="w-18 mb-5" src="/Logos/uber_logo_dark.png" alt="" />
+      <form action="" onSubmit={submitHandler}>
+            <h3 className="text-base font-medium mb-2 ">What's our Captain's Name</h3>
+        <div className='flex gap-2 mb-4'>
+       
+        <input 
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300  w-1/2 text-base placeholder:text-base"
+        required
+        type="text"
+        name="firstname"
+        value={firstname}
+        onChange={(e) => handleOnChange(e)}
+        placeholder="First Name"
+        />
+        <input 
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300  w-1/2 text-base placeholder:text-base"
+        required
+        type="text"
+        name="lastname"
+        value={lastname}
+        onChange={(e) => handleOnChange(e)}
+        placeholder="Last Name"
+        />
+
+
+ 
+        </div>
+
+        <h3 className="text-base font-medium mb-2 ">What's your Email</h3>
+        <input
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300 mb-4 w-full text-base placeholder:text-base"
+        required
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => handleOnChange(e)}
+        placeholder="email@eexample.com" 
+        />
+        <h3 className="text-base mb-2 font-medium">Enter Password</h3>
+        <input
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300 mb-4 w-full text-base placeholder:text-base"
+        type="password" 
+        name="password"
+        value={password}
+        onChange={(e) => handleOnChange(e)}
+        required 
+        placeholder="Password" 
+        />
+        <button className="bg-black text-white  w-full text-2xl rounded p-2 mt-5">Sign Up</button>
+        <p className="text-center mt-5"> New here?
+        <Link to="/captain/login" className="text-blue-600 text-lg font-medium mt-2 block">Already have an Account</Link>
+        </p>
+        
+      </form>
+      </div>
+      <div>
+        <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'> Google Privacy Policy</span>  and <span className='underline'>Terms of Service apply</span>.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default CaptainSignup
---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const UserLogin = () => {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(user);
+    }catch(err) {
+      console.log(err);
+    }finally{
+      setUser({
+        email: "",
+        password: "",
+      })
+    }
+    
+  }
+
+
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between ">
+      <div>
+      <img className="w-18 mb-5" src="/Logos/uber_logo_dark.png" alt="" />
+      <form action="" onSubmit={submitHandler}>
+        <h3 className="text-xl font-medium mb-2 ">What's your Email</h3>
+        <input 
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300 mb-4 w-full text-lg placeholder:text-base"
+        required 
+        type="email" 
+        name="email"
+        value={email}
+        onChange={(e) => handleOnChange(e)}
+        placeholder="email@eexample.com" 
+        />
+        <h3 className="text-xl mb-2 font-medium">Enter Password</h3>
+        <input 
+        className="bg-[#eeeeee] rounded px-4 py-2 border-2 border-gray-300 mb-4 w-full text-lg placeholder:text-base"
+        type="password" 
+        name="password"
+        value={password}
+        onChange={(e) => handleOnChange(e)}
+        required 
+        placeholder="Password" 
+        />
+        <button className="bg-black text-white  w-full text-2xl rounded p-2">Login</button>
+        <p className="text-center"> New here?
+        <Link to="/signup" className="text-blue-600 text-lg font-medium mt-2 block">Create New Account</Link>
+        </p>
+        
+      </form>
+      </div>
+      <div>
+        <Link to="/captain/login" className="bg-[#dd7973] text-white  w-full flex items-center justify-center text-2xl rounded p-2"> Sign in as Captain</Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogin;
